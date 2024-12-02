71cee2887a4f364fdabe55f73302620d7ba78425
fernandoabf <capcakeplay@hotmail.com>
2024-12-02 10:04:31 -0300

    Finished

--- src/services/IndexedDBService.js
+++ src/services/IndexedDBService.js
@@ -1,4 +1,5 @@
 export default {
+  // Função para inicializar o IndexedDB
   createIndexedDB() {
     const request = indexedDB.open("potholeDB", 1);
 
@@ -9,7 +10,7 @@ export default {
           keyPath: "id",
           autoIncrement: true,
         });
-        objectStore.createIndex("zipCode", "zipCode", { unique: false }); // Usar apenas o índice de CEP
+        objectStore.createIndex("zipCode", "zipCode", { unique: false });
       }
     };
 
@@ -22,6 +23,7 @@ export default {
     };
   },
 
+  // Função para salvar um buraco no banco de dados, associando um CEP
   saveBuracoToZipCode(zipCode, buraco) {
     const request = indexedDB.open("potholeDB", 1);
 
@@ -40,6 +42,7 @@ export default {
     };
   },
 
+  // Função para buscar buracos por CEP
   getPotholesByZipCode(zipCode) {
     return new Promise((resolve, reject) => {
       const request = indexedDB.open("potholeDB", 1);
@@ -65,5 +68,82 @@ export default {
         reject("Erro ao abrir o IndexedDB", event);
       };
     });
+  },
+
+  // Função para buscar todos os buracos (sem filtro por CEP)
+  getAllPotholes() {
+    return new Promise((resolve, reject) => {
+      const request = indexedDB.open("potholeDB", 1);
+
+      request.onsuccess = function (event) {
+        const db = event.target.result;
+        const transaction = db.transaction("potholes", "readonly");
+        const store = transaction.objectStore("potholes");
+
+        const getAllRequest = store.getAll();
+
+        getAllRequest.onsuccess = function () {
+          resolve(getAllRequest.result);
+        };
+
+        getAllRequest.onerror = function () {
+          reject("Erro ao buscar todos os buracos");
+        };
+      };
+
+      request.onerror = function (event) {
+        reject("Erro ao abrir o IndexedDB", event);
+      };
+    });
+  },
+
+  // Função para excluir um buraco por seu ID
+  deletePotholeById(id) {
+    const request = indexedDB.open("potholeDB", 1);
+
+    request.onsuccess = function (event) {
+      const db = event.target.result;
+      const transaction = db.transaction("potholes", "readwrite");
+      const store = transaction.objectStore("potholes");
+
+      const deleteRequest = store.delete(id); // Exclui o buraco pelo ID
+
+      deleteRequest.onsuccess = function () {
+        console.log("Buraco excluído com sucesso");
+      };
+
+      deleteRequest.onerror = function () {
+        console.error("Erro ao excluir o buraco", event);
+      };
+    };
+
+    request.onerror = function (event) {
+      console.error("Erro ao abrir o IndexedDB", event);
+    };
+  },
+
+  // Função para atualizar um buraco
+  updatePothole(buraco) {
+    const request = indexedDB.open("potholeDB", 1);
+
+    request.onsuccess = function (event) {
+      const db = event.target.result;
+      const transaction = db.transaction("potholes", "readwrite");
+      const store = transaction.objectStore("potholes");
+
+      const updateRequest = store.put(buraco); // Atualiza o buraco existente com a mesma chave
+
+      updateRequest.onsuccess = function () {
+        console.log("Buraco atualizado com sucesso");
+      };
+
+      updateRequest.onerror = function () {
+        console.error("Erro ao atualizar o buraco", event);
+      };
+    };
+
+    request.onerror = function (event) {
+      console.error("Erro ao abrir o IndexedDB", event);
+    };
   },
 };
